c9b9ed4370dc4aacb3b00ebb44d166b29a05b9c7
sevinchfarxodova <farxodovasevinch92@gmail.com>
2025-05-05 17:44:18 +0500

    core done

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,22 @@
+
+abstract class AppRoutesNames {
+  AppRoutesNames._();
+
+  static const String home = '/home';
+  static const String splash = '/splash';
+  static const String login = '/login';
+  static const String confirm = '/confirm';
+  static const String testUser = '/test_user';
+  static const String mustPay = '/must_pay';
+  static const String ratePage = '/rate_page';
+  static const String themePage = '/theme_page';
+  static const String profile = '/profile';
+  static const String lesson = '/lesson';
+  static const String testPage = '/testPage';
+  static const String language = '/language';
+  static const String resultPage = '/result_page';
+  static const String history = '/history';
+  static const String appTheme = '/app_theme';
+}
+
+
